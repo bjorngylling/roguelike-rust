93130196d864e8201e99e35ecd1be66feb5f8764
Björn Gylling <bjorn.gylling@gmail.com>
2022-11-21 18:13:37 +0100

    test: remove spritesheet tests

--- src/gfx.rs
+++ src/gfx.rs
@@ -174,37 +174,3 @@ impl SpriteSet {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn spriteset_src() {
-        let t = SpriteSet::new(4, 4, 10, 10);
-        assert_eq!(t.src(0, 0), graphics::Rect::new(0., 0., 0.25, 0.25));
-        assert_eq!(t.src(3, 0), graphics::Rect::new(0.75, 0., 0.25, 0.25));
-        assert_eq!(t.src(0, 1), graphics::Rect::new(0., 0.25, 0.25, 0.25));
-    }
-
-    #[test]
-    #[should_panic(expected = "accessing sprite outside sheet bounds at 4,0")]
-    fn spriteset_src_requesting_tile_outside_bounds() {
-        let t = SpriteSet::new(4, 4, 10, 10);
-        t.src(4, 0);
-    }
-
-    #[test]
-    fn spriteset_src_by_idx() {
-        let t = SpriteSet::new(4, 4, 10, 10);
-        assert_eq!(t.src_by_idx(0), graphics::Rect::new(0., 0., 0.25, 0.25));
-        assert_eq!(t.src_by_idx(3), graphics::Rect::new(0.75, 0., 0.25, 0.25));
-        assert_eq!(t.src_by_idx(4), graphics::Rect::new(0., 0.25, 0.25, 0.25));
-    }
-
-    #[test]
-    #[should_panic(expected = "accessing sprite by idx outside sheet bounds at 16")]
-    fn spriteset_src_by_idx_requesting_tile_outside_bounds() {
-        let t = SpriteSet::new(4, 4, 10, 10);
-        t.src_by_idx(16);
-    }
-}
